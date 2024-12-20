bd33fb063ac0949ef334bb963e2a9a10b341088b
Valerii <berezovskijvalera812@gmail.com>
2024-12-20 09:55:27 +0100

    cache

---
+++ README.md
@@ -0,0 +1,8 @@
+# Cache Library for Go
+
+A simple in-memory cache library for Go.
+
+## Installation
+
+```bash
+go get github.com/TODO/cache-lib
---
+++ cache.go
@@ -0,0 +1,21 @@
+package cache
+
+type Cache struct {
+	cache map[string]interface{}
+}
+
+func New() *Cache {
+	return &Cache{cache: make(map[string]interface{})}
+}
+
+func (c *Cache) Set(key string, value interface{}) {
+	c.cache[key] = value
+}
+
+func (c *Cache) Get(key string) interface{} {
+	return c.cache[key]
+}
+
+func (c *Cache) Delete(key string) {
+	delete(c.cache, key)
+}
